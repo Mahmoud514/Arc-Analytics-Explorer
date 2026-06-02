310edde907a95078c5087baab8f2a227efbd0c24
Mahmoud <50506318+Mahmoud514@users.noreply.github.com>
2026-06-02 14:41:47 +0300

    Update StatCard.tsx

--- components/StatCard.tsx
+++ components/StatCard.tsx
@@ -1,4 +1,3 @@
-
 type StatCardProps = {
   title: string;
   value: string | number;
@@ -9,23 +8,26 @@ export default function StatCard({
   value,
 }: StatCardProps) {
   return (
-<div className="
-  bg-slate-900
-  p-6
-  rounded-2xl
-  border
-  border-slate-800
-  hover:border-blue-500
-  hover:-translate-y-1
-  transition-all
-  duration-300
-">
+    <div
+      className="
+        bg-slate-900
+        p-6
+        rounded-2xl
+        border
+        border-slate-800
+        hover:border-blue-500
+        hover:-translate-y-1
+        transition-all
+        duration-300
+      "
+    >
+      <p className="text-slate-400 text-sm">
         {title}
-      </h2>
+      </p>
 
-<p className="text-4xl font-bold mt-3 tracking-tight">
-  {value}
-</p>
+      <p className="text-4xl font-bold mt-3 tracking-tight">
+        {value}
+      </p>
     </div>
   );
 }
